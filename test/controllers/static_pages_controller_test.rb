0322233bce6f0eb9668318942c316a147b574bcd
koratel <milo.elessar@gmail.com>
2015-01-25 14:22:35 +0000

    Added home and about pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Algorithm problem manager"
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+end
