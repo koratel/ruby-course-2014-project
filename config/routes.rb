2547c6d4f999023aa5e5e0db569509e9503b3fdb
koratel <milo.elessar@gmail.com>
2015-01-28 21:31:51 +0000

    Added password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root                'static_pages#home'
   get    'about'   => 'static_pages#about'
   get    'signup'  => 'users#new'
@@ -8,4 +12,5 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
