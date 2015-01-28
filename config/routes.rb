c928358cda5a4a6dff4092c1baa0340a6abbdf9b
koratel <milo.elessar@gmail.com>
2015-01-28 20:46:58 +0000

    Added account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  root 'static_pages#home'
-  get 'about'   => 'static_pages#about'
-  get 'signup'  => 'users#new'
+  root                'static_pages#home'
+  get    'about'   => 'static_pages#about'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
